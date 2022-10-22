200a96c8992f8661620e0efe134846423b908320
AlexeyOsipovKzn <osipov-leha@inbox.ru>
2022-10-22 16:24:35 +0300

    task 1

---
+++ Seminar_5/task_1.py
@@ -0,0 +1,14 @@
+from unittest import result
+
+
+def del_words(name):
+    words = name.split()
+    result = []
+    for item in words:
+        if 'абв' not in item:
+            result.append(item)
+    print('Слова без содержания \"абв\" -- >', *result)
+
+
+word = input('Введите слова через пробел:  ')
+del_words(word)
